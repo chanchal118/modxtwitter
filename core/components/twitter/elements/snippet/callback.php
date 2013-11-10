2ec9f9f05fcbb0af0709f0e768aa78a0575f0d5f
chanchal <chanchalmusic@gmail.com>
2013-11-10 21:16:27 +0600

    direct twitter implmentation

--- core/components/twitter/elements/snippet/callback.php
+++ core/components/twitter/elements/snippet/callback.php
@@ -16,6 +16,72 @@ if (!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empt
     $_SESSION['access_token'] = $access_token;
 // Let's get the user's info
     $user_info = $twitteroauth->get('account/verify_credentials');
+
+    if(isset($user_info->error)){
+        // Something's wrong, go back to square 1
+        header('Location: twitter_login.php');
+    } else {
+        $oauth_provider = 'twitter';
+
+        // Let's find the user by its ID
+        $sql = $modx->prepare('SELECT * FROM ext_twitter_users WHERE oauth_provider = :oauth_provider AND oauth_uid = :oauth_uid');
+        $sql->bindParam(':oauth_provider', $oauth_provider);
+        $sql->bindParam(':oauth_uid', $user_info->id);
+        $sql->execute();
+
+        if($sql->execute()) {
+
+            $result = $sql->fetch(PDO::FETCH_ASSOC);
+        } else {
+            $result = '';
+            echo $sql->errorCode();
+            print_r($sql->errorInfo());
+        }
+
+        // If not, let's add it to the database
+        if(empty($result)) {
+            $stmt = $modx->prepare('INSERT INTO ext_twitter_users (oauth_provider, oauth_uid, username, oauth_token, oauth_secret)
+                VALUES (:oauth_provider, :oauth_uid, :username, :oauth_token, :oauth_secret)');
+
+            $stmt->bindParam(':oauth_provider', $oauth_provider);
+            $stmt->bindParam(':oauth_uid', $user_info->id);
+            $stmt->bindParam(':username', $user_info->screen_name);
+            $stmt->bindParam(':oauth_token', $access_token['oauth_token']);
+            $stmt->bindParam(':oauth_secret', $access_token['oauth_token_secret']);
+
+            if ($stmt->execute()) {
+                $intLastId = $modx->lastInsertId();
+            } else {
+                $intLastId = '';
+                echo $stmt->errorCode();
+                print_r($stmt->errorInfo());
+            }
+
+            $sql = $modx->prepare('SELECT * from ext_twitter_users WHERE id = :id');
+            $sql->bindParam(':id', $intLastId);
+            $sql->execute();
+
+            $result = $sql->fetch(PDO::FETCH_ASSOC);
+
+        } else {
+            $intLastId = 8;
+            $sql = $modx->prepare('SELECT * from ext_twitter_users WHERE id = :id');
+            $sql->bindParam(':id', $intLastId);
+            $sql->execute();
+
+            $result = $sql->fetch(PDO::FETCH_ASSOC);
+        }
+
+        $_SESSION['id'] = $result['id'];
+        $_SESSION['username'] = $result['username'];
+        $_SESSION['oauth_uid'] = $result['oauth_uid'];
+        $_SESSION['oauth_provider'] = $result['oauth_provider'];
+        $_SESSION['oauth_token'] = $result['oauth_token'];
+        $_SESSION['oauth_secret'] = $result['oauth_secret'];
+
+        //header('Location: twitter_update.php');
+    }
+
 // Print user's info
 
     print_r($user_info);
